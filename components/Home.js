466f72a6d93da35e6e4dc804bad6fdde1b5ac87e
Wojtek Książek <wojtek99955@gmail.com>
2022-05-23 20:11:56 +0200

    text updatE

--- components/Home.js
+++ components/Home.js
@@ -3,11 +3,16 @@ import { StyleSheet, Text, View, Button } from "react-native";
 function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        onPress={() => navigation.navigate("About")}
-        title="Go to notifications"
-      />
+      <Text style={styles.mainText}>Welcome!</Text>
+      <Button onPress={() => navigation.navigate("About")} title="About us" />
     </View>
   );
 }
 export default Home;
+
+const styles = StyleSheet.create({
+  mainText: {
+    fontSize: 40,
+    marginBottom: 20,
+  },
+});
