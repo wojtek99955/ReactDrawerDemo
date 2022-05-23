0292c2163d02aa59c7708b1e72a57d516e04b837
Wojtek Książek <wojtek99955@gmail.com>
2022-05-23 20:40:23 +0200

    Btn styles update

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={styles.mainText}>Welcome!</Text>
-      <Button onPress={() => navigation.navigate("About")} title="About us" />
+      <TouchableOpacity
+        onPress={() => navigation.navigate("About")}
+        title="About us"
+        style={styles.button}
+      >
+        <Text style={styles.textBtn}>About Us</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,5 +20,15 @@ const styles = StyleSheet.create({
   mainText: {
     fontSize: 40,
     marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "blue",
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 10,
+  },
+  textBtn: {
+    fontSize: 20,
+    color: "white",
   },
 });
