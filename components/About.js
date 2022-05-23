2ea56bae3189c199759b1c2934b845372930976d
Wojtek Książek <wojtek99955@gmail.com>
2022-05-23 20:07:21 +0200

    About added

---
+++ components/About.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+function About({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+export default About;
